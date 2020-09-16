09f3d9479a9b54d2804fa289100eea79456cd71d
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-16 12:53:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,10 +8,10 @@ def batch_badge_creator(people)
     end
 end
 
-# def assign_rooms(speakers)
-#     new_arr = []
-#     speakers.each_with_index |name, index|
-#     new_arr << "Hello, #{name}! You'll be assinged to room #{index + 1}!"
-#   end
-#   new_arr
-# end
+def assign_rooms(people)
+    new_arr = []
+    speakers.each_with_index |name, index|
+    new_arr << "Hello, #{name}! You'll be assinged to room #{index + 1}!"
+  end
+  new_arr
+end
